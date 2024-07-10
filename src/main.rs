28e6bd43322396e7f43c321ccb923bbfb3c1774f
ashmeetsmonga <ashmeetsmonga@gmail.com>
2024-07-10 23:54:11 +0530

    add and list func added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut todo_list: Vec<Todo> = vec![];
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let inputs: Vec<String> = input.split_whitespace().map(str::to_string).collect();
+        match inputs[0].as_str() {
+            "add" => {
+                for v in inputs {
+                    let todo = Todo {
+                        title: v,
+                        is_completed: false,
+                    };
+                    todo_list.push(todo);
+                }
+            },
+            "list" => {
+                for todo in &todo_list {
+                    println!("{}", todo.title);
+                }
+            },
+            _ => println!("Match not found"),
+        }
+    }
+}
+
+struct Todo {
+    title: String,
+    is_completed: bool,
+}
+
+enum TODO_CASES {
+    
 }
